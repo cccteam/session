07002cee10498d91b11d20ee70d2dbfaad5144e1
hdishner-ccc <hunter@cloud-team.com>
2025-07-24 13:32:43 +0000

    Domain Specific Cookies

--- preauth_iface.go
+++ preauth_iface.go
@@ -1,7 +1,16 @@
 package session
 
+import (
+	"context"
+	"net/http"
+
+	"github.com/cccteam/ccc"
+)
+
 var _ PreAuthHandlers = &PreauthSession{}
 
 type PreAuthHandlers interface {
 	sessionHandlers
+	NewSession(ctx context.Context, w http.ResponseWriter, r *http.Request, username string) (ccc.UUID, error)
+	NewSessionWithDomain(ctx context.Context, w http.ResponseWriter, r *http.Request, username string, domain string) (ccc.UUID, error)
 }
