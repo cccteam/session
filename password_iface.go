2611012115140413134d6aeb11d37eeab10c0ee2
Joseph Watson <joseph@cloud-team.com>
2025-12-02 17:53:00 +0000

    Update PasswordHandlers

--- password_iface.go
+++ password_iface.go
@@ -10,7 +10,9 @@ var _ PasswordHandlers = &Password{}
 
 // PasswordHandlers defines the interface for password authentication handlers.
 type PasswordHandlers interface {
-	Login() http.HandlerFunc
+	Authenticated() http.HandlerFunc
 	ChangeUserPassword() http.HandlerFunc
+	Login() http.HandlerFunc
+	ValidateSession(next http.Handler) http.Handler
 	basesession.Handlers
 }
