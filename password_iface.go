ff2747ba7d538df41186655965e908c57497093a
Joseph Watson <joseph@cloud-team.com>
2025-12-02 17:38:27 +0000

    cleanup docs

--- password_iface.go
+++ password_iface.go
@@ -8,7 +8,7 @@ import (
 
 var _ PasswordHandlers = &Password{}
 
-// PasswordHandlers defines the interface for OIDC Azure session handlers.
+// PasswordHandlers defines the interface for password authentication handlers.
 type PasswordHandlers interface {
 	Login() http.HandlerFunc
 	ChangeUserPassword() http.HandlerFunc
